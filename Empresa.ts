7307f15a8b8df6a8cdc68392b847d60ce6b80f83
Guilherme Capriata <guilhermevcapriata@gmail.com>
2024-07-26 17:20:12 -0400

    teste

--- Empresa.ts
+++ Empresa.ts
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 let rafael: string = "mamador";
 =======
 let capri: string = "caneco";
@@ -7,4 +7,4 @@ let malaco = "gay";
 
 
 let malaquias="ser gay"
->>>>>>> eddef841c564d510c50376e2492b50d463343c67
+
