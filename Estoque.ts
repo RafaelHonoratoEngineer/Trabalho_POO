412c111741bd6ecf0bdb3982f587e7624b3497c3
Guilherme Capriata <guilhermevcapriata@gmail.com>
2024-07-27 15:06:56 -0400

    estoque

---
+++ Estoque.ts
@@ -0,0 +1,35 @@
+import { Produto } from "./Produto";
+
+export class Estoque{
+    private _idProduto : string;
+    private _quantidade : number;
+
+
+    constructor(_idProduto: string, _quantidade: number){
+        this._idProduto = _idProduto;
+        this._quantidade = _quantidade;
+
+    }
+    get idProduto(){
+        return this.idProduto;
+    }
+    set idProduto(idProduto: string){
+        this.idProduto = idProduto;
+    }
+    get quantidade(){
+        return this.quantidade;
+    }
+    set quantidade(quantidade: number){
+        this.quantidade = quantidade;
+    }
+
+    adicionarProduto(quantidade: number) {
+        this._quantidade += quantidade;
+    }
+
+    removerProduto(quantidade: number) {
+        if (this._quantidade >= quantidade) {
+            this._quantidade -= quantidade;
+        }         
+    }
+}
