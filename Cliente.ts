6f260d896eb22a0eca9c800a035196374f697a31
fabricioRCosta <fabricio.costa@sou.ufmt.br>
2024-07-30 19:00:32 -0400

    implementacao da classe vendedor e cliente

--- Cliente.ts
+++ Cliente.ts
@@ -1,3 +1,34 @@
-export class Cliente {
+import { Pessoa } from './Pessoa';
+
+export class Vendedor {
+
+    private _cliente: Pessoa;
     
+    constructor(nome: string, cpf: string, email: string) {
+        this._cliente = this._cliente;
+    }
+   
+    get nome(): string {
+        return this._cliente.nome;
+    }
+
+    set nome(nome: string) {
+        this._cliente.nome = nome;
+    }
+
+    get cpf(): string {
+        return this._cliente.cpf;
+    }
+
+    set cpf(cpf: string) {
+        this._cliente.cpf = cpf;
+    }
+
+    get email(): string {
+        return this._cliente.email;
+    }
+
+    set email(email: string) {
+        this._cliente.email = email;
+    }
 }
--- Vendedor.ts
+++ Vendedor.ts
@@ -1,3 +1,34 @@
+import { Pessoa } from './Pessoa';
+
 export class Vendedor {
 
+    private _vendedor: Pessoa;
+    
+    constructor(nome: string, cpf: string, email: string) {
+        this._vendedor = this._vendedor;
+    }
+   
+    get nome(): string {
+        return this._vendedor.nome;
+    }
+
+    set nome(nome: string) {
+        this._vendedor.nome = nome;
+    }
+
+    get cpf(): string {
+        return this._vendedor.cpf;
+    }
+
+    set cpf(cpf: string) {
+        this._vendedor.cpf = cpf;
+    }
+
+    get email(): string {
+        return this._vendedor.email;
+    }
+
+    set email(email: string) {
+        this._vendedor.email = email;
+    }
 }
