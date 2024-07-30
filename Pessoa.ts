db581c554847b162fd186fb63b5b1b4143186e4c
Guilherme Capriata <guilhermevcapriata@gmail.com>
2024-07-30 18:37:09 -0400

    classe pessoa

--- Pessoa.ts
+++ Pessoa.ts
@@ -1,3 +1,35 @@
 export class Pessoa {
-    
+    private _nome: string;
+    private _cpf: string;
+    private _email: string;
+
+    constructor(_nome: string, _cpf: string, _email: string) {
+        this._nome = _nome;
+        this._cpf = _cpf;
+        this._email = _email;
+    }
+
+    get nome() {
+        return this._nome;
+    }
+
+    set nome(nome: string) {
+        this._nome = nome;
+    }
+
+    get cpf() {
+        return this._cpf;
+    }
+
+    set cpf(cpf: string) {
+        this._cpf = cpf;
+    }
+
+    get email() {
+        return this._email;
+    }
+
+    set email(email: string) {
+        this._email = email;
+    }
 }
