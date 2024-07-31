7d48aaf7d7d6671b9277d1ce2a2a304fc77d3f31
RafaelHonoratoEngineer <rafael.honorato@sou.ufmt.br>
2024-07-31 10:51:08 -0400

    Alterações na venda

--- Venda.ts
+++ Venda.ts
@@ -11,13 +11,12 @@ export class Venda {
     private _vendedor: Vendedor;
     private _cliente: Cliente;
 
-    constructor (_produtos: Produto [], _formaPagamento: string, _preco:number, _dataVenda: Date, _lucro: number, _vendedor: Vendedor, _cliente: Cliente) {
+    constructor (_produtos: Produto [], _formaPagamento: string, _dataVenda: Date, _vendedor: Vendedor, _cliente: Cliente) {
     this._produtos = _produtos;
     this._formaPagamento = _formaPagamento;
-    this._preco = _preco;
-    this._dataVenda = _dataVenda;
-    this._lucro = _lucro;
+    this._dataVenda = new Date();
     this._vendedor = _vendedor;
+    this._cliente = _cliente;
     }
 
     get produtos () {
@@ -35,13 +34,33 @@ export class Venda {
     set formaPagamento (formaPagamento: string) {
         this._formaPagamento = formaPagamento;
     }
-
+    
     get preco () {
         return this._preco;
     }
 
-    set preco (preco: number) {
-        this._preco = preco;
+    precoVenda(){
+        let x;
+        if(this._formaPagamento == 'PIX'){ 
+
+            this._produtos.forEach(produto => {
+                x += produto.precoVenda;
+            })
+        }
+        
+        this._preco = x-(x*0.05);
+
+        return this._preco;
+    }
+        
+
+
+    get lucro () {
+        return this._lucro;
+    }
+
+    set lucro (lucro: number) {
+        this._lucro = lucro;
     }
 
     get dataVenda () {
@@ -50,14 +69,6 @@ export class Venda {
 
     set dataVenda (dataVenda: Date) {
         this._dataVenda = dataVenda;
-    }
-
-    get lucro () {
-        return this._lucro;
-    }
-
-    set lucro (lucro: number) {
-        this._lucro= lucro;
     }
 
     get cliente() {
